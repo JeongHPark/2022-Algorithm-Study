7c672a52704adb051849da68e1b4e189b8c20f9c
KimBohwa <102210861+Kimbohwa@users.noreply.github.com>
2022-10-14 19:27:32 +0900

    Update 같은 숫자는 싫어.java

--- Programmers/BoHwa/3주차(Stack, Queue)/같은 숫자는 싫어.java
+++ Programmers/BoHwa/3주차(Stack, Queue)/같은 숫자는 싫어.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         int[] answer;
-        ArrayList<Integer> arrList = new ArrayList<>();
+        ArrayList<Integer> arrList = new ArrayList<>(); //배열에 계속 추가하기 위해 ArrayList를 사용
         int num=-1;
 
         for (int i=0; i<arr.length; i++){
-            if (num!=arr[i]){ //마지막에 저장된 값과 현재 값이 같지 않으면 저장하고 num에 현재 숫자 저장
+            if (num!=arr[i]){ //마지막에 저장된 값과 현재 arr의 값이 같지 않으면 저장하고 num에 현재 숫자 저장
                 arrList.add(arr[i]);
                 num=arr[i];
             }
         }
 
-        answer=new int[arrList.size()];
+        answer=new int[arrList.size()]; //int형 배열에 ArrayList 값을 넣기
         for (int i=0; i<arrList.size(); i++){
             answer[i]=arrList.get(i);
         }
