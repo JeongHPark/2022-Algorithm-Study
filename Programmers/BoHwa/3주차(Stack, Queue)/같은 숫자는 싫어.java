4c2a7228d9873822b830274eeaf7f07fc458e685
KimBohwa <102210861+Kimbohwa@users.noreply.github.com>
2022-10-14 19:25:50 +0900

    Update 같은 숫자는 싫어.java

--- Programmers/BoHwa/3주차(Stack, Queue)/같은 숫자는 싫어.java
+++ Programmers/BoHwa/3주차(Stack, Queue)/같은 숫자는 싫어.java
@@ -1,19 +1,6 @@
 import java.util.*;
 
-public class 같은숫자는싫어 {
-    public static void main(String[] args){
-        Solution so = new Solution();
-        //so.solution();
-    }
-
-}
-
-
-//큐
-//같은 숫자 연속 -> 한 번만 출력
-//순서대로
-//마지막에 들어가 있는 것이 현재 숫자와 같으면 넣지 않고 같지 않으면 넣는다.
-class Solution {
+public class Solution {
     public int[] solution(int []arr) {
         int[] answer;
         ArrayList<Integer> arrList = new ArrayList<>();
