6cee2f8c5d98748f7c11a1a6fa24181e34b1fbf5
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-18 21:50:43 +0900

    Update 올바른 괄호.py

--- Programmers/YoungJun/3주차(Stack, Queue)/올바른 괄호.py
+++ Programmers/YoungJun/3주차(Stack, Queue)/올바른 괄호.py
@@ -1,9 +1,9 @@
 def solution(s):
     stack = 0 # ( 부호면 증가 ) 부호면 마이너스를 쌓기 위한 변수
-    for i in range(len(s)):
+    for i in s:
         if s[i] == '(':# ( 부호면 증가
             stack+=1
-        elif s[i]==')':# ) 부호면 마이너스
+        else :# ) 부호면 마이너스
             if stack<1:#1보다 작으면 탈락
                 return False
             else :
