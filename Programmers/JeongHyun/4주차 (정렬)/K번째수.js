3890be502739f8e7a0f564c61590b28077e206e1
JeongHPark <114078089+JeongHPark@users.noreply.github.com>
2022-10-19 19:32:10 +0900

    K번째수 - 정렬 (풀이 두개)

--- Programmers/JeongHyun/4주차 (정렬)/K번째수.js
+++ Programmers/JeongHyun/4주차 (정렬)/K번째수.js
@@ -16,3 +16,15 @@ function solution(array, commands) {
     }
     return answer;
 }
+
+
+function solution(array, commands) {
+    var answer = [];
+    answer = commands.map(a=>{
+        // map은 commands를 이용하여 answer을 만들어주는 함수로서
+        // a[0]-1 을 하면 commands[i][0] - 1로 범위만큼 array를 자릅니다.
+        // .sort를 통해 정렬한 뒤 a[2]-1 만을 리턴해줍니다. 이 과정을 commands의 length만큼 진행합니다.
+        return array.slice(a[0]-1,a[1]).sort((b,c)=>b-c)[a[2]-1];
+    })
+    return answer;
+}
