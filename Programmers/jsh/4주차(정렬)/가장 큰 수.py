3b49b03cdf1b4a0182caee471fe40ddc0e2c359f
wkdsh21 <100358810+wkdsh21@users.noreply.github.com>
2022-10-21 14:07:56 +0900

    Update 가장 큰 수.py

--- Programmers/jsh/4주차(정렬)/가장 큰 수.py
+++ Programmers/jsh/4주차(정렬)/가장 큰 수.py
@@ -3,7 +3,7 @@ def comparator(a,b):
     if a+b<b+a: #ex) '6','10' '610'과 '106'을 비교하여 순서를 바꿀지 정함 
         return -1 #-1 반환시 순서 바꿈
     else:
-        return 0 #그대로 둠
+        return 0 #그대로 둠 (처음에 else를 설정안했다가 오류가뜸 무조건 반환값이있어야함)
 
 def solution(numbers):
     n = [str(x) for x in numbers] #정렬을하기위해 정수를 문자로 변경
