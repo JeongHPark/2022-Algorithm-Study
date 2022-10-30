4af21e13a8ebaa1bccb70a5eae893bd4ea543cf7
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-30 16:55:13 +0900

    Update 네트워크.py
    deque 로 변경

--- Programmers/YoungJun/5주차 - 깊이,너비 우선 탐색(DFS,BFS)/네트워크.py
+++ Programmers/YoungJun/5주차 - 깊이,너비 우선 탐색(DFS,BFS)/네트워크.py
@@ -1,12 +1,13 @@
+from collections import deque
 def solution(n, computers):
     answer = 0
-    visited = [0 for i in range(n)] 
+    visited = [0 for i in range(n)]
     for i in range(n): # visited값에 0이 있을 시 anwer +=1 해주고 bfs 돌리기
         if visited[i]==0:
-            queue=[i]
+            queue=deque([i])
             visited[i]=1
             while queue:
-                v=queue.pop(0)
+                v=queue.popleft()
                 for j in range(n):
                     if visited[j]==0 and computers[v][j]==1:
                         queue.append(j)
