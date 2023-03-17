d3d367995eba2b5c4987711e8b99119c7479553c
KimBohwa <102210861+Kimbohwa@users.noreply.github.com>
2023-03-17 19:13:36 +0900

    Update 덧칠하기.py

--- Programmers/BoHwa/21주차/덧칠하기.py
+++ Programmers/BoHwa/21주차/덧칠하기.py
@@ -11,3 +11,17 @@ def solution(n, m, section):
                 arr[i]=True
 
     return answer
+
+
+'''
+다른 풀이
+참고 : https://comdoc.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%8D%A7%EC%B9%A0%ED%95%98%EA%B8%B0
+'''
+def solution(n, m, section):
+    answer = painted = 0
+    for s in section:
+        if s>=painted:
+            answer+=1
+            painted=s+m
+    
+    return answer
